e764d575261334c213e195ae7fe70877ad3c7a85
linusms <98586954+linusms@users.noreply.github.com>
2023-09-05 16:35:14 +0900

    Update 완주하지 못한 선수.py

--- 4. 딕셔너리(해시맵)/완주하지 못한 선수.py
+++ 4. 딕셔너리(해시맵)/완주하지 못한 선수.py
@@ -12,3 +12,31 @@ def solution(participant, completion):
             break
         
     return answer
+
+# 앞선 코드는 for 문 + .count() 함수로 시간복잡도 n^2
+# Counter 함수 사용해 시간복잡도를 줄인다
+
+from collections import Counter
+
+def solution(participant, completion):
+    complete_runner=Counter(completion)
+    
+    for name in participant:
+        if name in complete_runner and complete_runner[name]>0:
+            complete_runner[name]-=1
+        else:
+            return name
+
+
+# Counter 의 성질 이용
+
+from collection import Counter
+
+def solution(participant, completion):
+    participants=Counter(participant)
+    completes=Counter(completion)
+
+    # Counter는 +,-가 가능하고, value가 큰 순서대로 정렬되기 때문
+    return list(participants-completes)[0]
+
+
