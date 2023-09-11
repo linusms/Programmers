0de91b2ee065f8ec9c8ebb6ac9fe6c9d82acfb75
linusms <98586954+linusms@users.noreply.github.com>
2023-09-11 12:25:46 +0900

    Update 2차원으로 만들기.py

--- 3. 배열,리스트/2차원으로 만들기.py
+++ 3. 배열,리스트/2차원으로 만들기.py
@@ -7,3 +7,17 @@ def solution(num_list, n):
         answer.append(list(num_list[start:end]))
         start+=n
     return answer
+
+# 개선
+
+def solution(num_list,n):
+    idx=0
+    answer=[]
+    while idx<len(num_list):
+        answer.append([num_list[idx:idx+n]])
+    return answer
+
+# list comprehension 사용
+
+def solution(num_list,n):
+    return [[num_list[idx:idx+n]] for idx in range(0, len(num_list), n)]
