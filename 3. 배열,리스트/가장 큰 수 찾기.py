b5563362185012f242b2ba44b87f7721670e4bc0
linusms <98586954+linusms@users.noreply.github.com>
2023-09-11 12:34:16 +0900

    Update 가장 큰 수 찾기.py

--- 3. 배열,리스트/가장 큰 수 찾기.py
+++ 3. 배열,리스트/가장 큰 수 찾기.py
@@ -1,3 +1,19 @@
 def solution(array):
     answer=[max(array), array.index(max(array))]
     return answer
+
+# max()의 시간복잡도 O(n), 따라서 쓰지 않는 방법도 알아두기
+
+def solution(array):
+    largest=0
+    for i, num in enumerate(array):
+        if num>largest:
+            largest=num
+            largest_idx=i
+    return [largest, largest_idx]
+
+# 정렬로 풀기
+
+def solution(array):
+    num, i = sorted(enumerate(array), key=lambda x: x[1])[-1]
+    answer=[num,i]
