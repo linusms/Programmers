f87ef8197d7f860548ced336591e863d8aa576c2
linusms <98586954+linusms@users.noreply.github.com>
2023-09-11 12:21:59 +0900

    Update 잘라서 배열로 저장하기.py

--- 3. 배열,리스트/잘라서 배열로 저장하기.py
+++ 3. 배열,리스트/잘라서 배열로 저장하기.py
@@ -11,3 +11,18 @@ def solution(my_str, n):
         start+=n
 
     return answer
+
+# 굳이 start, end 둘 다 쓸 필요 없음
+# 인덱싱의 끝나는 부분은 자료의 길이 초과해도 상관없음
+
+def solution(my_str, n):
+    answer=[]
+    idx=0
+    while idx<len(my_str):
+        answer.append(my_str[idx:idx+n])
+    return answer
+
+# list comprehension 사용
+
+def solution(my_str,n):
+    return [my_str[idx:idx+n] for idx in range(0,len(my_str),n)]
